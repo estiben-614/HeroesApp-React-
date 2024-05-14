3a3ee192b23d69f43e622f178848653617155bb0
Estiben Fernandez Echeverri <efernandeze@unal.edu.co>
2024-05-14 00:06:56 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://estiben-614.github.io/heroesApp/",
+  assetsInclude: ["**/.JPG"],
+  base: "./",
 });
